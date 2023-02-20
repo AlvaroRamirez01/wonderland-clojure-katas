ae4a61461d0055549a8b23909033f032e49d4e4a
Alvaro Ramirez(Huananzhi-Xeon-Win10) <alvaro_rl@ciencias.unam.mx>
2023-02-20 17:13:34 -0600

    alphabet_cipher solution success

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,20 @@
 (ns alphabet-cipher.coder)
+;; Ramirez Lopez Alvaro
+
+;; Da el indice de una letra en el alfabeto
+(defn indice [c] (- (int c) (int \a)))
+
+;; Usando aritmetica modular, intercambia una letra por otra dependiendo 
+;; si se cifrando o descifrando
+(defn mod26 [op char1 char2] 
+  (char (+ (mod (op (indice char1) (indice char2)) 26) (int \a))))
 
 (defn encode [keyword message]
-  "encodeme")
+  (apply str (map #(mod26 + %1 %2) (cycle keyword) message)))
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply str (map #(mod26 - %2 %1) (cycle keyword) message)))
 
 (defn decipher [cipher message]
-  "decypherme")
-
+  (let [keystream (apply str (map #(mod26 - %1 %2) cipher message))]
+    (first (filter #(= message (decode % cipher)) (map #(subs keystream 0 %) (range 1 (count keystream)))))))
